0798883b3bd6324f26e399c65951a503bd8d6285
Mhretaab Berhe <mhretberhe@gmail.com>
2024-09-05 13:04:15 +0200

    null safety

--- src/main/kotlin/com/example/basic/main.kt
+++ src/main/kotlin/com/example/basic/main.kt
@@ -1,5 +1,12 @@
 package com.example.basic
 
 fun main(args: Array<String>) {
-    println("Hello World!")
+    var name: String = "Martha"
+    //name = null--->Error
+
+    var brand: String? = null
+    println( brand?.uppercase())
+
+    brand = "Nike"
+    println( brand?.uppercase())
 }
