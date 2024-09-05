0cf9a47d5a0cf3f98a70a697cadbb74e061ba857
Mhretaab Berhe <mhretberhe@gmail.com>
2024-09-05 15:59:30 +0200

    math operations

--- src/main/kotlin/com/example/basic/main.kt
+++ src/main/kotlin/com/example/basic/main.kt
@@ -1,5 +1,23 @@
 package com.example.basic
 
+import kotlin.math.*
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val num1 = 10
+    val num2 = 3
+
+    println("$num1 + $num2 = ${num1 + num2}")
+    println("$num1 - $num2 = ${num1 - num2}")
+    println("$num1 / $num2 = ${num1 / num2}")
+    println("$num1 * $num2 = ${num1 * num2}")
+    println("$num1 % $num2 = ${num1 % num2}")
+
+    //from the math package
+    println("PI: ${kotlin.math.PI}")
+    println("|-30| = ${kotlin.math.abs(-30)}")
+    println("cos(180) = ${kotlin.math.cos(PI)}")
+    println("max(5, 3) = ${max(5, 3)}")
+    println("round(2.238) ~ ${round(2.238)}")
+    println("ceil(2.238) ~ ${ceil(2.238)}")
+    println("floor(2.238) ~ ${floor(2.238)}")
 }
