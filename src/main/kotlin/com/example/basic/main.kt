e7de7d06d9dd3b62e902c0f576e95e3ceb20d9d2
Mhretaab Berhe <mhretberhe@gmail.com>
2024-09-05 12:11:22 +0200

    type inference, val vs var, const vs val

--- src/main/kotlin/com/example/basic/main.kt
+++ src/main/kotlin/com/example/basic/main.kt
@@ -1,5 +1,20 @@
 package com.example.basic
 
+const val PI = 3.14 //for values that are already known at compile time or not computed. and also similiar to java static final
+//const val PI = getPi()---> error
+val pi = getPi()
+
+fun getPi(): Double = 3.14
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    //type inference
+    var name = "John"
+    var age = 35
+
+    // val vs var
+    var currency = "Euro" //mutable
+    currency = "Dollar"
+
+    val nationality = "USA" //readonly
+    //nationality = "UK" --> error
 }
