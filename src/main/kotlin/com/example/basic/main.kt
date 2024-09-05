be3170e3bef95416011cf4c74948333b97be62fd
Mhretaab Berhe <mhretberhe@gmail.com>
2024-09-05 11:51:31 +0200

    vaiables and data types

--- src/main/kotlin/com/example/basic/main.kt
+++ src/main/kotlin/com/example/basic/main.kt
@@ -1,5 +1,18 @@
 package com.example.basic
 
 fun main(args: Array<String>) {
-    println("Hello World!")
+    //variables
+    val name: String = "Jamila"
+    var salary: Double = 7000.0
+
+    //basic data types
+    var int: Int = 10
+    var long: Long = 1000000000000000L
+    var float: Float = 12.3f
+    var double: Double = 12.5
+    var short: Short = 10
+    var byte: Byte = 127
+    var char: Char = 'A'
+    var string: String = "Name"
+    var bool: Boolean = true
 }
