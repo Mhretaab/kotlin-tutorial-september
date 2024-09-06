1b830dd35f5ac8b2fec297a66897d7702b9461cb
Mhretaab Berhe <mhretberhe@gmail.com>
2024-09-06 11:25:44 +0200

    data type conversion

--- src/main/kotlin/com/example/basic/main.kt
+++ src/main/kotlin/com/example/basic/main.kt
@@ -1,5 +1,17 @@
 package com.example.basic
 
+import java.math.MathContext
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val number = "10"
+
+    println(10 + number.toInt())
+    println(10.toString() + number)
+
+    val d = 10.5
+    println(d.toFloat())
+    println(d.toInt())
+    println(d.toBigDecimal(MathContext(2)))
+    println(d.toBits())
+    println(d.toUInt())
 }
